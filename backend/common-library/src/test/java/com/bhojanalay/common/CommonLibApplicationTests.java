174d6d2299894690e35bcf514e1d620e9cd2d6ff
saurabh23patre93 <63465162+saurabh23patre93@users.noreply.github.com>
2026-07-20 00:14:36 +0530

    module setting

---
+++ backend/common-library/src/test/java/com/bhojanalay/common/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhojanalay.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
